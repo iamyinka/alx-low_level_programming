f0783bf0f4aede1ee3048ee713310faf3bb4cc62
Yinka Ash <perpetualcoder@gmail.com>
2022-12-13 03:53:15 -0800

    Finish Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Print "ALX" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	char alx[] = "ALX";
+	int i = 0;
+
+	while (alx[i] != '\0')
+	{
+		_putchar(alx[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
 
