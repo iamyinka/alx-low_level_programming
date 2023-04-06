30f7d394d2741cf923d66e7a48e0de3f37971957
Yinka Ash <perpetualcoder@gmail.com>
2023-04-06 15:10:18 -0700

    Task done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,54 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - creates a new node in the hashtable
- * @ht: pointer to the hashtable
- * @key: key
- * @value: value to place in the node
- * Return: 1  in success 0 otherwise
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-	unsigned long int haidx;
-	hash_node_t *temp;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (ht == NULL || key == '\0' || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	if (new_node == NULL)
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	haidx = key_index((unsigned char *)key, ht->size);
-	if (ht->array[haidx] != NULL)
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		temp = ht->array[haidx];
-		while (temp != NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			if (strcmp(temp->key, new_node->key) == 0)
-				break;
-			temp = temp->next;
-		}
-		if (temp == NULL)
-		{
-			new_node->next = ht->array[haidx];
-			ht->array[haidx] = new_node;
-		}
-		else
-		{
-			free(temp->value);
-			temp->value = strdup(new_node->value);
-			free(new_node->value);
-			free(new_node->key);
-			free(new_node);
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
-	else
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		new_node->next = NULL;
-		ht->array[haidx] = new_node;
+		free(value_copy);
+		return (0);
 	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
